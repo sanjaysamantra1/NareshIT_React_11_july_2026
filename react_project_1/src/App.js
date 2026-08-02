f250813a42d58f541712b2ecd111e37d276cc7e0
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-02 11:57:26 +0530

    react project

---
+++ react_project_1/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Categories from './components/categories/Categories';
+import Carousel from './components/carousel/Carousel';
+import Body from './components/body/Body';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Categories />
+      <Carousel />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
