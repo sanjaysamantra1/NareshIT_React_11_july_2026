78226a8cfe2b447ca0f8ecb923a41a4ee3eac02c
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-02 12:48:21 +0530

    footer

--- react_project_1/src/components/footer/Footer.js
+++ react_project_1/src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="container-fluid">
-      <footer className="py-3 my-4 bg-dark text-white">
+      <footer className="py-3 bg-dark text-white">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item"><a href="#" className="nav-link px-2">Home</a></li>
           <li className="nav-item"><a href="#" className="nav-link px-2">Features</a></li>
