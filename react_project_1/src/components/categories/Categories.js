536b8620725c31552a2851786112b39800dfb810
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-02 12:22:20 +0530

    categories

--- react_project_1/src/components/categories/Categories.js
+++ react_project_1/src/components/categories/Categories.js
@@ -2,6 +2,45 @@ import React from 'react'
 
 export default function Categories() {
   return (
-    <div>Categories</div>
+   <div className="container">
+     <div className='row'>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/fashion.svg' />
+            <div>Fashion</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/mobiles.svg' />
+            <div>Mobile</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/electronics.svg' />
+            <div>Electronics</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/fashion.svg' />
+            <div>Fashion</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/mobiles.svg' />
+            <div>Mobile</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/electronics.svg' />
+            <div>Electronics</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/fashion.svg' />
+            <div>Fashion</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/mobiles.svg' />
+            <div>Mobile</div>
+        </div>
+        <div className='col'>
+            <img src='https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/electronics.svg' />
+            <div>Electronics</div>
+        </div>
+     </div>
+   </div>
   )
 }
